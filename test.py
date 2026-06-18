bfa301a55b2e8acb8a694d02e2f262ce8f62df9d
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-06-19 02:45:06 +0600

    try to new

--- test.py
+++ test.py
@@ -1,5 +1,26 @@
+# import pandas as pd
+
+# students = pd.Series(["Nazmul", "Rahim", "Karim"])
+
+# print(students)
+
 import pandas as pd
 
-students = pd.Series(["Nazmul", "Rahim", "Karim"])
+students = {
+    "Name": ["Nazmul", "Rahim", "Karim", "Sakib"],
+    "Age": [22, 23, 21, 24],
+    "Marks": [85, 75, 90, 80]
+}
 
-print(students)
+df = pd.DataFrame(students)
+
+print("Student Information")
+print(df)
+
+print("\nAverage Marks:", df["Marks"].mean())
+print("Highest Marks:", df["Marks"].max())
+
+top_students = df[df["Marks"] >= 80]
+
+print("\nTop Students:")
+print(top_students)
